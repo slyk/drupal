b1a26e44220363a7b0b49c3700b66dfea6df844c
Kirill [TooPro] Kuzmitskyy <slykirill@gmail.com>
2020-12-18 14:11:22 +0200

    initial commit

---
+++ index2.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * The routines here dispatch control to the appropriate handler, which then
+ * prints the appropriate page.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ */
+
+phpinfo();
+die();
+
+/**
+ * Root directory of Drupal installation.
+ */
+define('DRUPAL_ROOT', getcwd());
+
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+//menu_execute_active_handler();
+
+//echo ' t='.tps_transaction_cron();
