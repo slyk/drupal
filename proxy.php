5b023bcee1676f4d2345f3397202cbf62a29585b
Kirill [TooPro] Kuzmitskyy <slykirill@gmail.com>
2021-09-16 10:22:22 +0300

    - add proxy to make requests from flash to other websites

---
+++ proxy.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * used to load external resources, because sfw flash files cant do this because of policy issues
+ * need to add crossdomain.xml to all websites root whre we need to download files or use this proxy
+ */
+
+$srv = $_GET['srv'];
+$path= $_GET['path'];
+
+if($srv=='tpsadminfuncs') $srv = 'http://srv1.toopro.org:3088/'; else die();
+
+$url = $srv . $path;
+echo file_get_contents($url);
